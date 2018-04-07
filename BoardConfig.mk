17e26c5a5e9a0570285f443303b6650dc911efb6
Ather <ather90@gmail.com>
2018-04-07 23:58:47 +0000

    Oreo Source Upload

---
+++ BoardConfig.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := device/samsung/gts28wifi
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineage_exynos5433_gts28wifi_defconfig
+#TARGET_KERNEL_CONFIG := cm_exynos5433-gts28wifi_defconfig
+#BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(LOCAL_PATH)/dtb.img
+
+# Recovery
+TARGET_OTA_ASSERT_DEVICE := gts28wifi,gts28wifixx
+
+# Inherit common board flags
+include device/samsung/gts2-common/BoardConfigCommon.mk
---
+++ device.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := device/samsung/gts28wifi
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    ueventd.universal5433.rc
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+# Overlay
+#DEVICE_PACKAGE_OVERLAYS += device/samsung/gts28wifi/overlay
+	
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/gts28wifi/gts28wifi-vendor.mk)
